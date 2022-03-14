e81c30b7a37fb38ef5535a2b6764b2eef741b30a
dhimaseka <dimaseka83@gmail.com>
2022-03-14 10:28:37 +0700

    ++

---
+++ pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Hallo, Dhimas, It,s Page Contact</h1>
+        <nuxt-link to="/">back</nuxt-link>
+    </div>
+</template>
---
+++ pages/user/_id.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Profile {{$route.params.id}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    validate({params}){
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
